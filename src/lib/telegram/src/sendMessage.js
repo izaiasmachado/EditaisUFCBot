01eb8a8b7d714a8c59793022555158ca2aa05c18
Izaias Machado <izaiasmachado@icloud.com>
2020-10-31 21:49:25 -0300

    fix: axios get params

--- src/lib/telegram/src/sendMessage.js
+++ src/lib/telegram/src/sendMessage.js
@@ -2,6 +2,12 @@ const api = require('../util/api')
 const { TELEGRAM_CHANNEL_ID } = process.env
 
 module.exports = async ({ text, parse_mode }) => {
-    const { data } = await api.get(`sendMessage?chat_id=${TELEGRAM_CHANNEL_ID}&text=${text}&parse_mode=${parse_mode}`)    
+    const { data } = await api.get(`sendMessage`, {
+        params: {
+            chat_id: TELEGRAM_CHANNEL_ID,
+            text,
+            parse_mode
+        }
+    })    
     return data
 }
